f9bda98aa87ff66b78de37a0b6b142af7f54c73d
suleiman gacheru <dretrevor8@gmail.com>
2023-08-01 14:25:37 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require ('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/scripts.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      child_process: false,
+      fs: false,
+      os: false,
+      path: false,
+    },
+  },
+
+};
