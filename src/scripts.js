2c961c7b8b7b024acd8f8323b576a919f162cd93
suleiman gacheru <dretrevor8@gmail.com>
2023-08-02 13:54:13 +0300

    enable get data from API

--- src/scripts.js
+++ src/scripts.js
@@ -1,78 +1,51 @@
-const template = [
-    { title: "movie 1", rating: 4.7 },
-    { title: "movie 2", rating: 4.0 },
-    { title: "movie 3", rating: 5.0 }
-  ];
-  
-  const display = () => {
-    const dataList = document.getElementsByClassName('list-1');
-    dataList.innerHtml = "";
-  
-    template.forEach((item) => {
-      const list_item = document.createElement('li');
-      list_item.textContent = `title: ${item.title}, rating: ${item.rating}`;
-  
-      dataList.appendChild(list_item);
-    });
-  };
-  
-  const fetchAndDisplayShows = async () => {
-    try {
-      const response = await fetch("https://api.tvmaze.com/shows");
-      const data = await response.json();
-      displayShows(data);
-    } catch (error) {
-      console.log('Error fetching data:', error);
+import './styles.css';
+
+const BASE_API_URL = 'https://api.tvmaze.com/shows';
+const showsPerPage = 10;
+let currentPage = 0;
+
+const getShowsEndpoint = (page) => `${BASE_API_URL}?page=${page}`;
+
+const fetchAndDisplayShows = async () => {
+  try {
+    const response = await fetch(getShowsEndpoint(currentPage));
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-  };
-  
-  const displayShows = (shows) => {
-    const showListElement = document.getElementById("showList");
-  
-    // Clear any existing data in the list
-    showListElement.innerHTML = "";
-  
-    // Loop through the TV shows array and create HTML elements dynamically
-    shows.forEach((show) => {
-      const showItem = createShowItem(show);
-      showListElement.appendChild(showItem);
-    });
-  };
-  
-  // Function to create a single TV show item with title, image, and buttons
-  const createShowItem = (show) => {
-    const showItemElement = document.createElement("div");
-    showItemElement.classList.add("show-item");
-  
-    const titleElement = document.createElement("h2");
-    titleElement.textContent = show.name;
-  
-    const imageElement = document.createElement("img");
-    imageElement.src = show.image.medium;
-    imageElement.alt = show.name;
-  
-    const commentsButton = document.createElement("button");
-    commentsButton.textContent = "Comments";
-    commentsButton.disabled = true; // This button does nothing for now
-    // Optionally, you can add an event listener to handle the button click
-     commentsButton.addEventListener("click", handleCommentsButtonClick);
-  
-    const reservationsButton = document.createElement("button");
-    reservationsButton.textContent = "Reservations";
-    reservationsButton.disabled = true; // This button does nothing for now
-    // Optionally, you can add an event listener to handle the button click
-    // reservationsButton.addEventListener("click", handleReservationsButtonClick);
-  
-    showItemElement.appendChild(titleElement);
-    showItemElement.appendChild(imageElement);
-    showItemElement.appendChild(commentsButton);
-    showItemElement.appendChild(reservationsButton);
-  
-    return showItemElement;
-  };
-  
-  // When the page loads, fetch data and display the shows
-  window.onload = () => {
-    fetchAndDisplayShows();
-  };
-  
+    const data = await response.json();
+    const listElement = document.querySelector('.list-1');
+
+    // Clear the existing list before displaying new shows
+    clearList();
+
+    // Display only the first 10 shows
+    for (let i = 0; i < showsPerPage; i++) {
+      const show = data[i];
+      if (!show) break; // If there are fewer than 10 shows, break out of the loop
+      const listItem = document.createElement('li');
+      listItem.textContent = show.name;
+      listElement.appendChild(listItem);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+const clearList = () => {
+  const listElement = document.querySelector('.list-1');
+  while (listElement.firstChild) {
+    listElement.removeChild(listElement.firstChild);
+  }
+};
+
+const fetchNextPage = () => {
+  currentPage++;
+  fetchAndDisplayShows();
+};
+
+const loadMoreButton = document.getElementById('load-more-button');
+loadMoreButton.addEventListener('click', fetchNextPage);
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetchAndDisplayShows();
+});
