bcb42e63bf3b59e9c26ea76b4dcd2787929bfc3b
Newton Ombese <newtonombese1@gmail.com>
2023-08-02 09:54:51 +0300

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require ('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -38,5 +38,4 @@ module.exports = {
       path: false,
     },
   },
-
 };
