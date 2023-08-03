9c3d32f6acbb0727f5264b9583e75bf849a3a4c4
suleiman gacheru <dretrevor8@gmail.com>
2023-08-03 15:04:09 +0300

    fix eslint

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,31 @@ let currentPage = 0;
 
 const getShowsEndpoint = (page) => `${BASE_API_URL}?page=${page}`;
 
+const getItemLikesEndpoint = (appId, itemId) => `${INVOLVEMENT_API_URL}/apps/${appId}/likes?item_id=${itemId}`;
+
+const getItemLikes = async (appId, itemId) => {
+  try {
+    const response = await fetch(getItemLikesEndpoint(appId, itemId));
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data.length > 0 ? data[0].likes : 0;
+  } catch (error) {
+    console.error('Error:', error);
+    return 0;
+  }
+};
+
+const fetchLikesForAllShows = async (appId, data) => {
+  const likesMap = new Map();
+  for (const show of data) {
+    const likes = await getItemLikes(appId, show.id);
+    likesMap.set(show.id, likes);
+  }
+  return likesMap;
+};
+
 const fetchAndDisplayShows = async () => {
   try {
     const response = await fetch(getShowsEndpoint(currentPage));
@@ -15,9 +40,11 @@ const fetchAndDisplayShows = async () => {
     }
     const data = await response.json();
     const listElement = document.querySelector('.list-1');
-    // clearList();
 
-    for (let i = 0; i < showsPerPage; currentPage += 1) {
+    const appId = 'abc234'; // Your app's unique identifier (replace with the actual identifier)
+    const likesMap = await fetchLikesForAllShows(appId, data);
+
+    for (let i = 0; i < showsPerPage; i += 1) {
       const show = data[i];
       if (!show) break;
 
@@ -27,7 +54,7 @@ const fetchAndDisplayShows = async () => {
       const summaryElement = document.createElement('p');
       const premiereDateElement = document.createElement('p');
       const commentBtn = document.createElement('button');
-      const heartIcon = document.createElement('i'); // Heart icon element
+      const heartIcon = document.createElement('i');
       const timify = document.createElement('span');
       imageElement.src = show.image && show.image.medium ? show.image.medium : 'placeholder.png';
       imageElement.alt = show.name;
@@ -36,31 +63,31 @@ const fetchAndDisplayShows = async () => {
       summaryElement.textContent = show.summary || 'No summary available';
       premiereDateElement.textContent = show.premiereDate || 'No premiere date available';
       commentBtn.textContent = 'comment here';
-      commentBtn.classList.add('comment-btn'); // Assigning class to the comment button
+      commentBtn.classList.add('comment-btn');
+
+      // Get likes for the current item from the pre-fetched likes map
+      const likes = likesMap.get(show.id);
+
+      // Add likes to the list item
+      const likesElement = document.createElement('span');
+      likesElement.textContent = `Likes: ${likes}`;
 
       // Add heart icon to the list item
-      //
       heartIcon.classList.add('ti-heart', 'icon-heart');
       timify.appendChild(heartIcon);
-      // Add the heart icon to the list item before the comment button
+      // Add the heart icon and likes to the list item before the comment button
       listItem.appendChild(imageElement);
       listItem.appendChild(titleElement);
       listItem.appendChild(commentBtn);
-      listElement.appendChild(listItem);
+      listItem.appendChild(likesElement);
       listItem.appendChild(timify);
+      listElement.appendChild(listItem);
     }
   } catch (error) {
     console.error('Error:', error);
   }
 };
 
-/* const clearList = () => {
-  const listElement = document.querySelector('.list-1');
-  while (listElement.firstChild) {
-    listElement.removeChild(listElement.firstChild);
-  }
-};
-*/
 const fetchNextPage = () => {
   currentPage += 1;
   fetchAndDisplayShows();
@@ -71,32 +98,4 @@ loadMoreButton.addEventListener('click', fetchNextPage);
 
 window.addEventListener('DOMContentLoaded', () => {
   fetchAndDisplayShows();
-});
-
-const navMenus = document.querySelector('.nav-menu');
-const openHamburger = document.querySelector('.mobile-menu-icon');
-const closeHamburger = document.querySelector('.mobile-menu-close-icon');
-const mobileMenuLinks = document.querySelectorAll('.mobile-links');
-
-openHamburger.addEventListener('click', () => {
-  navMenus.classList.add('show');
-  openHamburger.classList.add('hide');
-  closeHamburger.classList.add('show');
-  document.body.style.overflow = 'hidden';
-});
-
-closeHamburger.addEventListener('click', () => {
-  navMenus.classList.remove('show');
-  openHamburger.classList.remove('hide');
-  closeHamburger.classList.remove('show');
-  document.body.style.overflow = 'auto';
-});
-
-mobileMenuLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    navMenus.classList.remove('show');
-    openHamburger.classList.remove('hide');
-    closeHamburger.classList.remove('show');
-    document.body.style.overflow = 'auto';
-  });
 });
