fffb311424db1e519cb1acff8531c327ea0b4020
suleiman gacheru <dretrevor8@gmail.com>
2023-08-04 18:58:36 +0300

    make changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,64 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/scripts.js',
-  },
-  mode: 'development',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][text]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
-  optimization: {
-    runtimeChunk: 'single',
+  performance: {
+    hints: false,
   },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      child_process: false,
-      fs: false,
-      os: false,
-      path: false,
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
     },
   },
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vidvib',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
 };
